f514e75fd40379b8e8d1166f0b8afd485565d39d
Jonathan Dueck <jdd612@gmail.com>
2019-12-12 16:09:36 -0700

    Impl filtering, soring, limiting of tour data

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,21 +5,45 @@ const Tour = require('./../models/tourModel');
 // );
 
 exports.getAllTours = async (req, res) => {
-  try {
-    const tours = await Tour.find();
-    res.status(200).json({
-      status: 'success',
-      results: tours.length,
-      data: {
-        tours
-      }
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err
-    });
+  // BUILD QUERY
+  // (1A) FILTERING
+
+  const queryObj = { ...req.query };
+  const excludedFields = ['page', 'sort', 'limit', 'fields'];
+  excludedFields.forEach(el => delete queryObj[el]);
+
+  // (1B) ADVANCED FILTERING
+  let queryStr = JSON.stringify(queryObj);
+  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+  let query = Tour.find(JSON.parse(queryStr));
+
+  // (2) SORTING
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
   }
+
+  // (3) FIELD LIMITING
+  if (req.query.fields) {
+    const fields = req.query.fields.split(',').join(' ');
+    query = query.select(fields);
+  } else {
+    query = query.select('-__v');
+  }
+  // EXECUTE QUERY
+  const tours = await query;
+
+  // SEND RESPONSE
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  });
 };
 
 exports.getTour = async (req, res) => {
